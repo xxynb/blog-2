43b216d1b6d6ea1f74fa2e8359b5dd9e1d300979
xxynb <1203244060@qq.com>
2022-05-28 16:27:26 +0800

    修复

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 
-const userBase = 'blog';
+const userBase = 'blog-2';
 
 export default defineUserConfig({
   lang: "zh-CN",
